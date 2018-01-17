363bebacc4aa1c18d4c9e7e836113fa27c048720
yyud <990715561@qq.com>
2018-01-17 17:54:56 +0800

    udate

--- src/main/java/com/issc/sendmessage/service/Star.java
+++
@@ -1,9 +0,0 @@
-package com.issc.sendmessage.service;
-
-import com.issc.sendmessage.Utils.TimingUtil;
-
-public class Star {
-    public static void main(String[] args) {
-        TimingUtil.timer();
-    }
-}
